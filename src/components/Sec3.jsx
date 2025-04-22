926c03e10acc127924c286be60a50d33eadd4948
BrunoDV <suruxd@outlook.com>
2025-04-21 22:49:06 -0500

    changues

--- src/components/Sec3.jsx
+++ src/components/Sec3.jsx
@@ -3,7 +3,7 @@ function Sec3() {
     <>
 
         <div className="bg-gradient-to-t from-[#011345] to-[#023BC6] w-full h-screen flex items-center justify-center m-0 m-auto">
-        <div className="rounded-full flex items-center w-[90%] h-[50%] md:w-[70%] md:h-[90%] xl:w-[40%] xl:h-[80%] justify-center bg-[url('/homaro/images/img4.png')] bg-cover bg-center">
+        <div className="rounded-full flex items-center w-[90%] h-[50%] md:w-[70%] md:h-[90%] xl:w-[40%] xl:h-[80%] justify-center bg-[url('images/img4.png')] bg-cover bg-center">
         <h1 className="text-3xl xl:text-5xl sm:px-64 leading-tight px-20 xl:px-48 transparent"> Experiencia
  en todas
  las industrias
