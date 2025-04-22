6370600564ec90a7c71433200074422b712664f2
BrunoDV <suruxd@outlook.com>
2025-04-21 20:14:06 -0500

    modifications

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './assets/animations.css';
 function App() {
   return (
       <MyState>
-        <Router basename="/doc-api">
+        <Router basename="/homaro">
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
