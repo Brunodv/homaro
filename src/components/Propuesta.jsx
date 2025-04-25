a3c7796f5bef6ab4b1cdb95e3678a694acbd0c70
BrunoDV <suruxd@outlook.com>
2025-04-25 11:13:47 -0500

    changues responsive

--- src/components/Propuesta.jsx
+++ src/components/Propuesta.jsx
@@ -3,24 +3,24 @@ import { TfiPlus } from "react-icons/tfi";
 
 function Propuesta() {
   return (
-    <div className="w-[90%] min-h-screen px-6 py-12 md:px-32 md:py-24 text-black m-auto bg-gray-200 rounded-3xl gap-20 flex flex-col xl:gap-0 ">
+    <div className="w-[90%] min-h-screen px-6 py-12 text-black   m-auto bg-gray-200 rounded-3xl gap-12 flex flex-col xl:gap-0 md:py-24 md:gap-18">
       {/* Título y párrafo */}
       <div className="w-full">
-        <h2 className="text-2xl font-bold text-blue-700 mb-4">PROPUESTA</h2>
-        <p className="text-xl sm:text-2xl md:text-4xl w-full sm:w-[90%] xl:w-[65%] leading-snug text-gray-800 font-light">
+        <h2 className="text-xl px-2 font-bold text-blue-700 mb-8 sm:px-14 md:text-2xl md:px-16 lg:px-22 xl:px-22 2xl:text-xl 2xl:font-bold 2xl:mb-2 ">PROPUESTA</h2>
+        <p className="text-lg px-2 sm:text-2xl md:text-3xl w-full sm:w-[90%] xl:w-[80%] 2xl:w-[60%] leading-snug text-gray-800 font-light sm:m-0 sm:m-auto sm:px-8 xl:m-0 xl:px-22">
           Soluciones complementarias que se integran para crear respuestas completas, versátiles y alineadas con los desafíos reales de las organizaciones.
         </p>
       </div>
 
       {/* Tarjetas */}
-      <div className="flex flex-col lg:flex-row gap-4 xl:gap-18 justify-center">
+      <div className="flex flex-col gap-12 w-full sm:items-center m-0 m-auto lg:w-[70%]  md:gap-18 xl:gap-14 2xl:gap-17 xl:my-0  xl:flex-row xl:w-[90%]">
         {/* Tarjeta 1 */}
         <div
-          className="relative w-full sm:w-[80%] md:w-[90%] xl:w-[30%] h-[440px]"
+          className="relative w-full sm:w-[80%] md:w-[80%] xl:w-[30%] h-[340px] sm:h-[440px] md:h-[640px] xl:h-[340px] 2xl:h-[440px]"
           style={{ perspective: 1000 }}
         >
           <motion.div
-            className="w-full h-full relative cursor-pointer border border-blue-600 rounded-3xl xl:mt-20"
+            className="w-full h-full relative cursor-pointer border border-blue-600 rounded-3xl xl:mt-20 "
             whileHover={{ rotateY: 180 }}
             whileTap={{ rotateY: 180 }}
             transition={{ duration: 0.8, ease: "easeInOut" }}
@@ -34,19 +34,19 @@ function Propuesta() {
               </div>
 
               {/* Título en la parte inferior */}
-              <p className="text-blue-800 font-semibold text-3xl px-4 select-none">Consultoría</p>
+              <p className="text-blue-800 font-normal text-2xl px-4 select-none md:text-4xl">Consultoría</p>
             </div>
 
             {/* Cara trasera */}
             <div className="absolute w-full h-full backface-hidden rotate-y-180 flex items-center justify-center bg-blue-600 text-white rounded-3xl">
-              <p className="text-xl font-bold select-none">Más información</p>
+              <p className="text-2xl font-bold select-none md:text-4xl">Más información</p>
             </div>
           </motion.div>
         </div>
 
         {/* Tarjeta 2 */}
         <div
-          className="relative w-full sm:w-[80%] md:w-[90%] xl:w-[30%] h-[440px] rounded-3xl"
+          className="relative w-full sm:w-[80%] md:w-[90%] xl:w-[30%] h-[340px] sm:h-[440px]  md:h-[640px] xl:h-[340px] 2xl:h-[440px] rounded-3xl"
           style={{ perspective: 1000 }}
         >
           <motion.div
@@ -69,19 +69,19 @@ function Propuesta() {
               </div>
 
               {/* Título en la parte inferior */}
-              <p className="text-white font-semibold text-3xl px-4 select-none">Educación</p>
+              <p className="text-white font-normal text-2xl px-4 select-none md:text-4xl">Educación</p>
             </div>
 
             {/* Cara trasera */}
             <div className="absolute w-full h-full backface-hidden rotate-y-180 flex items-center justify-center bg-orange-700 text-white rounded-3xl">
-              <p className="text-xl font-bold select-none">Más sobre Educación</p>
+              <p className="text-xl font-bold select-none md:text-4xl">Más sobre Educación</p>
             </div>
           </motion.div>
         </div>
 
         {/* Tarjeta 3 - estática pero con flip */}
         <div
-          className="relative w-full sm:w-[80%] md:w-[90%] xl:w-[30%] h-[440px]"
+          className="relative w-full sm:w-[80%] md:w-[90%] xl:w-[30%] h-[340px] sm:h-[440px] md:h-[640px] xl:h-[340px] 2xl:h-[440px]"
           style={{ perspective: 1000 }}
         >
           <motion.div
@@ -99,12 +99,12 @@ function Propuesta() {
               </div>
 
               {/* Título en la parte inferior */}
-              <p className="text-blue-800 font-semibold text-3xl px-4 select-none">Plataformas</p>
+              <p className="text-blue-800 font-normal text-2xl px-4 select-none md:text-4xl">Plataformas</p>
             </div>
 
             {/* Cara trasera */}
             <div className="absolute w-full h-full backface-hidden rotate-y-180 flex items-center justify-center bg-blue-600 text-white rounded-3xl">
-              <p className="text-xl font-bold select-none">Explora más plataformas</p>
+              <p className="text-xl font-bold select-none md:text-4xl">Explora más plataformas</p>
             </div>
           </motion.div>
         </div>
--- src/components/Sec5.jsx
+++ src/components/Sec5.jsx
@@ -1,9 +1,11 @@
 function Sec5() {
   return (
-    <div className="xl:min-h-screen flex items-center justify-center">
-  <div className="w-[90%] h-[70%] xl:w-[80%] bg-gray-100 rounded-3xl flex flex-col xl:flex-row items-center gap-12 ">
-    
-    <p className="w-[80%] xl:w-[60%] text-gray-700 pt-16 xl:pl-16 xl:pb-16 text-2xl xl:text-4xl font-light">
+    <div className="min-h-screen flex items-center justify-center">
+  <div className="w-[90%] h-[90%] bg-gray-100 rounded-3xl flex flex-col lg:flex-row-reverse xl:w-[80%] 2xl:w-[67%]">
+<div className="flex items-start w-full h-[100%] lg:w-[50%] lg:m-0 lg:m-auto lg:py-6 lg:mr-8 xl:mr-10">
+<img src="images/img5.png" className="rounded-t-2xl object-cover w-full h-full md:h-[340px] xl:h-[430px] lg:rounded-2xl" alt="" />
+</div>
+    <div className="lg:flex lg:items-center lg:jusfify-center lg:p-5 lg:w-[90%]"><p className="text-gray-700 text-xl sm:text-2xl px-8 py-4 lg:px-6 lg:text-2xl font-light xl:text-3xl 2xl:p-18">
     Nuestra pasión es ayudar a organizaciones 
 —y a las personas dentro de ellas— a 
 evolucionar. Trabajamos para que cada 
@@ -13,9 +15,7 @@ eficiente, sostenible y humana. Porque al
 transformar la manera en que trabajamos, 
 también<span className="text-green-600 italic font-extrabold">transformamos el mundo que 
 habitamos.</span>
-    </p>
-
-    <img src="images/img5.png" className="w-[90%] xl:w-[40%] xl:h-[80%] object-cover m-10 rounded-3xl" alt="" />
+    </p></div>
     
   </div>
 </div>
