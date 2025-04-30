2dc90694a23589fa97f57234917af53a5540edfc
BrunoDV <suruxd@outlook.com>
2025-04-29 20:01:33 -0500

    animaciones

--- src/components/Sec5.jsx
+++ src/components/Sec5.jsx
@@ -5,7 +5,7 @@ function Sec5() {
 <div className="flex items-start w-full h-[100%] lg:w-[50%] lg:m-0 lg:m-auto lg:py-6 lg:mr-8 xl:mr-10">
 <img src="images/img5.png" className="rounded-t-2xl object-cover w-full h-full md:h-[340px] xl:h-[430px] lg:rounded-2xl" alt="" />
 </div>
-    <div className="lg:flex lg:items-center lg:jusfify-center lg:p-5 lg:w-[90%]"><p className="text-gray-700 text-xl sm:text-2xl px-8 py-4 lg:px-6 lg:text-2xl font-light xl:text-3xl 2xl:p-18">
+    <div className="bg-red-300 lg:flex lg:items-center lg:jusfify-center lg:p-5 lg:w-[90%]"><p className="text-gray-700 text-xl sm:text-2xl px-8 py-4 lg:px-6 lg:text-2xl font-light xl:text-3xl 2xl:p-18">
     Nuestra pasión es ayudar a organizaciones 
 —y a las personas dentro de ellas— a 
 evolucionar. Trabajamos para que cada 
