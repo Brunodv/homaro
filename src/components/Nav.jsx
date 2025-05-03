0021187d816564e5cfe688d1024008a1adf34ceb
BrunoDV <suruxd@outlook.com>
2025-05-02 23:18:15 -0500

    menú al 100%

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -26,30 +26,17 @@ function Nav() {
     <AnimatePresence>
         {isMenuOpen && (
           <motion.div
-            className="fixed inset-0 z-2 flex items-center justify-end xl:pr-4 xl:py-[2%]"
-            onClick={() => setIsMenuOpen(false)} // Cierra el menú si se hace clic en el fondo
+            className="fixed inset-0 z-2 flex items-center justify-end"
+            onClick={() => setIsMenuOpen(false)} 
           >
             <motion.div
-              initial={{ scaleY: 0, width: '0%', originY: 0 }}  // Comienza con 100% en móviles
-              animate={{
-                scaleY: 1,
-                width: isMenuOpen
-                  ? window.innerWidth >= 1024  // Si la pantalla es grande (mayor o igual a 1024px)
-                    ? '20%'                    // Ancho reducido para pantallas grandes
-                    : '100%'                    // Ancho mayor para móviles y pantallas pequeñas
-                  : '0%',
-                originY: 0,
-              }}
-              exit={{
-                scaleY: 0,
-                width: '15%',
-                originY: 0
-              }}
-              transition={{ duration: 0.4 }}
-              className="bg-[#e6fbff] overflow-hidden h-full z-2 shadow-lg flex flex-col p-6 rounded-xl 
-                sm:w-[60%] lg:w-[80%] w-full"  // Responsivo: 100% en móviles, 60% en sm+, 80% en lg+
-              onClick={(e) => e.stopPropagation()} // Evitar que el clic en el menú cierre el menú
-            >
+  initial={{ x: '30%' }}       // Comienza fuera, a la derecha
+  animate={{ x: 0 }}            // Se mueve hacia la izquierda al mostrarse
+  exit={{ x: '100%' }}          // Vuelve a la derecha al cerrarse
+  transition={{ duration: 0.3 }}
+  className="bg-[#e6fbff] overflow-hidden h-full z-2 shadow-lg flex flex-col p-6 rounded-l-xl 
+    sm:w-[60%] lg:w-[20%] w-full">
+
               <div className="flex justify-end w-full">
                 <button onClick={() => setIsMenuOpen(false)} className="text-3xl text-[#011957] flex items-center text-base cursor-pointer">
                   <IoMdClose />
